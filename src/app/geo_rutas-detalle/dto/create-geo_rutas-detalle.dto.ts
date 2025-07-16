031313e9a7c855942083c39c3c7b1f9e9a623701
Emmanuel Manzanilla <pcoriente.dev.jrs@gmail.com>
2025-07-16 16:45:02 -0600

    detalles

--- src/app/geo_rutas-detalle/dto/create-geo_rutas-detalle.dto.ts
+++ src/app/geo_rutas-detalle/dto/create-geo_rutas-detalle.dto.ts
@@ -3,17 +3,17 @@ import { IsDateString, IsInt, IsNotEmpty, IsNumber } from 'class-validator';
 export class CreateGeoRutaDetalleDto {
   @IsInt()
   @IsNotEmpty()
-  readonly idRuta: number;
+  idRuta: number;
 
   @IsNumber({}, { message: 'La latitud debe ser un número.' })
   @IsNotEmpty({ message: 'La latitud es requerida.' })
-  readonly latitud: number;
+  latitud: number;
 
   @IsNumber({}, { message: 'La longitud debe ser un número.' })
   @IsNotEmpty({ message: 'La longitud es requerida.' })
-  readonly longitud: number;
+  longitud: number;
 
   @IsDateString({}, { message: 'fecha_hora debe ser una fecha válida.' })
   @IsNotEmpty({ message: 'fecha_hora es requerida.' })
-  readonly fecha_hora: Date;
+  fecha_hora: Date;
 }
