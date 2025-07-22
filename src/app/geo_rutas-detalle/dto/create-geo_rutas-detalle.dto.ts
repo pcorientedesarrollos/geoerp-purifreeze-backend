b0654830bc76996179bceb6261666b0a66bac243
Angel_Heredia_Cardos <aheredia@soft-box.com.mx>
2025-07-22 16:41:00 -0600

    update

--- src/app/geo_rutas-detalle/dto/create-geo_rutas-detalle.dto.ts
+++ src/app/geo_rutas-detalle/dto/create-geo_rutas-detalle.dto.ts
@@ -3,14 +3,14 @@ import { IsDateString, IsInt, IsNotEmpty, IsNumber } from 'class-validator';
 export class CreateGeoRutaDetalleDto {
   @IsInt()
   @IsNotEmpty()
-  idRuta: number;
+ idRuta: number;
 
   @IsNumber({}, { message: 'La latitud debe ser un número.' })
   @IsNotEmpty({ message: 'La latitud es requerida.' })
-  latitud: number;
+   latitud: number;
 
   @IsNumber({}, { message: 'La longitud debe ser un número.' })
   @IsNotEmpty({ message: 'La longitud es requerida.' })
-  longitud: number;
+   longitud: number;
 
 }
